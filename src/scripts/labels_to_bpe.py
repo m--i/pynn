f80e7c2e510ed96ce0f6dc0a16f53ed2fb46390e
Maximilian Awiszus <maximilian.awiszus@student.kit.edu>
2020-06-22 18:47:42 +0200

    BugFix in bpe conversion script

--- src/scripts/labels_to_bpe.py
+++ src/scripts/labels_to_bpe.py
@@ -9,20 +9,20 @@ import sentencepiece as spm
 from os import listdir
 from os.path import isfile, join
 
-parser = argparse.ArgumentParser(description='Conver an audio file to wav in parallel.')
+parser = argparse.ArgumentParser(description='Convert an audio file to wav in parallel.')
 parser.add_argument('--vocabulary', help='path to vocabulary text file', type=str, default=None)
 parser.add_argument('--model', help='path to already trained BPE model', type=str, default=None)
-parser.add_argument('--label', help='path to label file', type=int, default=1)
-parser.add_argument('--out', help='path to output file', type=int, default='tr-label.bpe')
+parser.add_argument('--label', help='path to label file', type=str)
+parser.add_argument('--out', help='path to output file', type=str, default='tr-label.bpe')
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    if args.voabulary is not None:
-        spm.SentencePieceTrainer.train(input=args.vocabulary, model_prefix='m', vocab_size=32000, character_coverage=0.9995, model_type='bpe', bos_id=-1, eos_id=-1)
+    if args.vocabulary is not None:
+        spm.SentencePieceTrainer.train(input=args.vocabulary, model_prefix='bpe', vocab_size=8000, character_coverage=0.9995, model_type='bpe', bos_id=-1, eos_id=-1)
     elif args.model is not None:
         sp = spm.SentencePieceProcessor(model_file=args.model)
         with open(args.label, 'r') as label_file:
             with open(args.out, 'w') as bpe_file:
                 for label in label_file:
-                    bpe_file.write(' '.join([str(i) for i in sp.encode(label)]))
+                    bpe_file.write(' '.join([str(i) for i in sp.encode(label)]) + '\n')
 
