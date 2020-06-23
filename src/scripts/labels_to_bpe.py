ef7df25fcc81c7b9ce4eb1ca3c7f589ee1eef9f8
Maximilian Awiszus <maximilian.awiszus@kit.edu>
2020-06-23 16:13:02 +0200

    added conversion to dict in bpe creation

--- src/scripts/labels_to_bpe.py
+++ src/scripts/labels_to_bpe.py
@@ -15,10 +15,17 @@ parser.add_argument('--model', help='path to already trained BPE model', type=st
 parser.add_argument('--label', help='path to label file', type=str)
 parser.add_argument('--out', help='path to output file', type=str, default='tr-label.bpe')
 
+MODEL_PREFIX = 'bpe'
+
 if __name__ == '__main__':
     args = parser.parse_args()
     if args.vocabulary is not None:
-        spm.SentencePieceTrainer.train(input=args.vocabulary, model_prefix='bpe', vocab_size=8000, character_coverage=0.9995, model_type='bpe', bos_id=-1, eos_id=-1)
+        spm.SentencePieceTrainer.train(input=args.vocabulary, model_prefix=MODEL_PREFIX, vocab_size=4000, character_coverage=1.0, model_type='bpe', bos_id=-1, eos_id=-1)
+        with open(MODEL_PREFIX + '.vocab', 'r') as bpe_vocab_file:
+            with open(MODEL_PREFIX + '.dict', 'w') as bpe_dict_file:
+                for i, line in enumerate(bpe_vocab_file):
+                    token = line.split()[0]
+                    bpe_dict_file.write('{} {}\n'.format(token, i))
     elif args.model is not None:
         sp = spm.SentencePieceProcessor(model_file=args.model)
         with open(args.label, 'r') as label_file:
