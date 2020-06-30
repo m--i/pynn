36ac15c8eb1ad55d4ce17d6cc503836f8f41e414
Maximilian Awiszus <maximilian.awiszus@student.kit.edu>
2020-06-30 17:09:17 +0200

    added parameters for voc size + char coverage

--- src/scripts/labels_to_bpe.py
+++ src/scripts/labels_to_bpe.py
@@ -14,13 +14,15 @@ parser.add_argument('--sentences', help='path to sentences file', type=str, defa
 parser.add_argument('--model', help='path to already trained bpe model', type=str, default=None)
 parser.add_argument('--label', help='path to label file', type=str, default='train_cleaned.txt')
 parser.add_argument('--out', help='path to output file', type=str, default='tr-label.bpe')
+parser.add_argument('--vocab-size', help='size of vocabulary', type=int, default=40000)
+parser.add_argument('--character-coverage', help='coverage for characters', type=float, default=0.9995)
 
 MODEL_PREFIX = 'bpe'
 
 if __name__ == '__main__':
     args = parser.parse_args()
     if args.sentences is not None:
-        spm.SentencePieceTrainer.train(input=args.sentences, model_prefix=MODEL_PREFIX, vocab_size=4000, character_coverage=0.9995, model_type='bpe', bos_id=-1, eos_id=-1)
+        spm.SentencePieceTrainer.train(input=args.sentences, model_prefix=MODEL_PREFIX, vocab_size=args.vocab_size, character_coverage=args.character_coverage, model_type='bpe', bos_id=-1, eos_id=-1)
         print('Saving dict: {}.dict'.format(MODEL_PREFIX))
         with open(MODEL_PREFIX + '.vocab', 'r') as bpe_vocab_file:
             with open(MODEL_PREFIX + '.dict', 'w') as bpe_dict_file:
@@ -33,5 +35,10 @@ if __name__ == '__main__':
             with open(args.out, 'w') as bpe_file:
                 for line in label_file:
                     utt, sent = line.split(maxsplit=1)
-                    bpe_file.write(utt + '\t' + ' '.join([str(i) for i in sp.encode(sent)]) + '\n')
+                    sent = sent.replace('\n','')
+                    encoding = sp.encode(sent)
+                    # ToDo: something wrong with sentencepiece here?
+                    #if encoding[0] == 3:
+                    #    encoding = encoding[1:]
+                    bpe_file.write(utt + ' ' + ' '.join([str(id) for id in encoding]) + '\n')
 
